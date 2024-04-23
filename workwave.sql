d14ded07606f2185839b2bfc8634c6a22ee41321
Arthur Ferreira <111396211+Arthur-Ferreira-Fernades@users.noreply.github.com>
2024-04-23 20:38:24 -0300

    Update 1.6
    Atualizando o script do banco de dados

--- workwave.sql
+++ workwave.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 20-Abr-2024 às 01:00
+-- Tempo de geração: 24-Abr-2024 às 01:37
 -- Versão do servidor: 10.4.27-MariaDB
 -- versão do PHP: 8.2.0
 
@@ -24,15 +24,62 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `oculpante`
+-- Estrutura da tabela `alugar`
 --
 
-CREATE TABLE `oculpante` (
+CREATE TABLE `alugar` (
+  `AluId` int(11) NOT NULL,
+  `AluDataEntrada` date NOT NULL,
+  `AluDataSaida` date NOT NULL,
+  `OcuId` int(11) NOT NULL,
+  `EspId` int(11) NOT NULL,
+  `PlaId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `espaco`
+--
+
+CREATE TABLE `espaco` (
+  `EspId` int(11) NOT NULL,
+  `EspNome` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `EspEndereco` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `EspDescricao` text NOT NULL,
+  `EspCapacidade` int(11) NOT NULL,
+  `EspDisponibilidade` tinyint(1) NOT NULL DEFAULT 1,
+  `EspDataCadastro` date NOT NULL,
+  `ProId` int(11) NOT NULL,
+  `SerId` int(11) NOT NULL,
+  `PlaId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `ocupante`
+--
+
+CREATE TABLE `ocupante` (
   `OcuId` int(11) NOT NULL,
   `OcuNome` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `OcuSenha` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `OcuEmail` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `OcuTelefone` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `planosprecos`
+--
+
+CREATE TABLE `planosprecos` (
+  `PlaId` int(11) NOT NULL,
+  `PlaDiariaValor` double NOT NULL,
+  `PlaSemanal` double DEFAULT NULL,
+  `PlaMensalValor` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
@@ -56,15 +103,54 @@ CREATE TABLE `proprietario` (
 INSERT INTO `proprietario` (`ProId`, `ProNome`, `ProSenha`, `ProEmail`, `ProTelefone`) VALUES
 (1, 'Arthur', '123', 'arthur@123.com', '11986599562');
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `servamenidades`
+--
+
+CREATE TABLE `servamenidades` (
+  `SerId` int(11) NOT NULL,
+  `SerWifi` tinyint(1) DEFAULT 0,
+  `SerArcondicionado` tinyint(1) DEFAULT 0,
+  `SerBebedouro` tinyint(1) DEFAULT 0,
+  `SerComputadores` tinyint(1) DEFAULT 0,
+  `SerCozinha` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `oculpante`
+-- Índices para tabela `alugar`
 --
-ALTER TABLE `oculpante`
+ALTER TABLE `alugar`
+  ADD PRIMARY KEY (`AluId`),
+  ADD KEY `PlaId` (`PlaId`),
+  ADD KEY `OcuId` (`OcuId`),
+  ADD KEY `EspId` (`EspId`);
+
+--
+-- Índices para tabela `espaco`
+--
+ALTER TABLE `espaco`
+  ADD PRIMARY KEY (`EspId`),
+  ADD KEY `PlaId` (`PlaId`),
+  ADD KEY `ProId` (`ProId`),
+  ADD KEY `SerId` (`SerId`);
+
+--
+-- Índices para tabela `ocupante`
+--
+ALTER TABLE `ocupante`
   ADD PRIMARY KEY (`OcuId`);
+
+--
+-- Índices para tabela `planosprecos`
+--
+ALTER TABLE `planosprecos`
+  ADD PRIMARY KEY (`PlaId`);
 
 --
 -- Índices para tabela `proprietario`
@@ -73,20 +159,70 @@ ALTER TABLE `proprietario`
   ADD PRIMARY KEY (`ProId`);
 
 --
+-- Índices para tabela `servamenidades`
+--
+ALTER TABLE `servamenidades`
+  ADD PRIMARY KEY (`SerId`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT de tabela `oculpante`
+-- AUTO_INCREMENT de tabela `alugar`
 --
-ALTER TABLE `oculpante`
+ALTER TABLE `alugar`
+  MODIFY `AluId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `espaco`
+--
+ALTER TABLE `espaco`
+  MODIFY `EspId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `ocupante`
+--
+ALTER TABLE `ocupante`
   MODIFY `OcuId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `planosprecos`
+--
+ALTER TABLE `planosprecos`
+  MODIFY `PlaId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `proprietario`
 --
 ALTER TABLE `proprietario`
   MODIFY `ProId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `servamenidades`
+--
+ALTER TABLE `servamenidades`
+  MODIFY `SerId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `alugar`
+--
+ALTER TABLE `alugar`
+  ADD CONSTRAINT `alugar_ibfk_1` FOREIGN KEY (`PlaId`) REFERENCES `planosprecos` (`PlaId`),
+  ADD CONSTRAINT `alugar_ibfk_2` FOREIGN KEY (`OcuId`) REFERENCES `ocupante` (`OcuId`),
+  ADD CONSTRAINT `alugar_ibfk_3` FOREIGN KEY (`EspId`) REFERENCES `espaco` (`EspId`);
+
+--
+-- Limitadores para a tabela `espaco`
+--
+ALTER TABLE `espaco`
+  ADD CONSTRAINT `espaco_ibfk_1` FOREIGN KEY (`PlaId`) REFERENCES `planosprecos` (`PlaId`),
+  ADD CONSTRAINT `espaco_ibfk_2` FOREIGN KEY (`ProId`) REFERENCES `proprietario` (`ProId`),
+  ADD CONSTRAINT `espaco_ibfk_3` FOREIGN KEY (`SerId`) REFERENCES `servamenidades` (`SerId`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
