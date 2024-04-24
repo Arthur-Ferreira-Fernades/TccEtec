75700157be574d044d298b88a74f4ae965a5ad11
Arthur Ferreira <111396211+Arthur-Ferreira-Fernades@users.noreply.github.com>
2024-04-23 21:45:07 -0300

    Update 2.2
    Adicionando um botão de logoff e direcionando para login no index

---
+++ TCC/ValidaCadastro.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/validaCadastro.css">
+    <title>Cadastro</title>
+</head>
+<body>
+  <div class="container" id="container">
+    <div class="form-container sign-in">
+        <?php
+          try {
+              $conexao = new PDO("mysql:host=localhost; dbname=workwave", "root", "");
+              
+              $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          } catch (PDOException $erro) {
+              echo "Erro na conexão:" . $erro->getMessage();
+          }
+          
+          //valores do html e colocados em variaveis do php//
+          $Nome = $_POST['Nome'];
+          $Senha= $_POST['Senha'];
+          $Email = $_POST['Email'];
+          $Telefone = $_POST['Telefone'];
+          $ProprietarioLocador = $_POST['ProprietarioLocador'];
+
+          //tratamento de erro caso nao encontre a tabela//
+          if ($Nome != "" && $Senha != "" && $Email!= "" && $Telefone != "" && $ProprietarioLocador != "" && $ProprietarioLocador == "Locatario") {
+              try{
+
+                    //Insert Into - comandos do SQL //
+                    //variavel $stmt com a conexao prepara o comando insert para o banco //
+                    // values com ponto de interrogação se referem a quantidade de item a serem inseridos no banco//
+                  $stmt = $conexao->prepare("INSERT INTO Ocupante (OcuNome, OcuSenha , OcuEmail, OcuTelefone) VALUES (?, ?, ?, ?)");
+                  
+                  //valores passados por parametros//
+                  $stmt->bindParam(1, $Nome);
+                  $stmt->bindParam(2, $Senha);
+                  $stmt->bindParam(3, $Email);
+                  $stmt->bindParam(4, $Telefone);
+                  
+                  //executa o comando no banco //
+                  $stmt->execute();
+                
+                  //se executa comando e inseriu corretamente contagem de linhas será maior que 0 - com isso cadastrado //
+                  //senao executou e nao retornou nenhuma linha - algum erro no código//
+                      if ($stmt->rowCount() > 0) {
+                  
+                      //verifica campos se está tudo ok sem  nulo //
+                        echo "Dados cadastrados com sucesso!";
+                        $id = null;
+                        $Nome = null;
+                        $Email = null;
+                        $Telefone = null;
+                  
+                        //senao exibe erro ao cadastrar//
+                    } else {
+                        echo "Erro ao tentar efetivar cadastro";
+                    }
+                  
+                  
+                  //ira exibir o tipo de erro//
+              }catch (PDOException $erro) {
+                  
+                  echo "Erro: " . $erro->getMessage();
+                  
+              }
+                  
+            } else if ($Nome != "" && $Senha != "" && $Email!= "" && $Telefone != "" && $ProprietarioLocador != "" && $ProprietarioLocador == "Proprietario") {
+              try{
+
+                  //Insert Into - comandos do SQL //
+                  //variavel $stmt com a conexao prepara o comando insert para o banco //
+                  // values com ponto de interrogação se referem a quantidade de item a serem inseridos no banco//
+                $stmt = $conexao->prepare("INSERT INTO Proprietario (ProNome, ProSenha , ProEmail, ProTelefone) VALUES (?, ?, ?, ?)");
+                
+                //valores passados por parametros//
+                $stmt->bindParam(1, $Nome);
+                $stmt->bindParam(2, $Senha);
+                $stmt->bindParam(3, $Email);
+                $stmt->bindParam(4, $Telefone);
+                
+                //executa o comando no banco //
+                $stmt->execute();
+              
+                //se executa comando e inseriu corretamente contagem de linhas será maior que 0 - com isso cadastrado //
+                //senao executou e nao retornou nenhuma linha - algum erro no código//
+                  if ($stmt->rowCount() > 0) {
+                
+                    //verifica campos se está tudo ok sem  nulo //
+                      echo "Dados cadastrados com sucesso!";
+                      $id = null;
+                      $Nome = null;
+                      $Email = null;
+                      $Telefone = null;
+                
+                      //senao exibe erro ao cadastrar//
+                  } else {
+                      echo "Erro ao tentar efetivar cadastro";
+                  }
+                
+                //ira exibir o tipo de erro//
+                }catch (PDOException $erro) {
+                
+                echo "Erro: " . $erro->getMessage();
+                
+                }
+                } else {
+                  echo "Cadastrar dados validos";
+                }
+        ?>
+        <button>
+          <a href="login.php">Voltar</a>
+        </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ TCC/login.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Login.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container" id="container">
+        <div class="form-container sign-up">
+            <form action="ValidaLogin.php" method="post">
+                <img src="img/WorkWaveLogoSemFundo.png" width = "200" class = "imagem">
+                <input type="email" name="login" placeholder = "Email">
+                <input type="password" name="senha" placeholder = "Senha">
+                <?php
+                    session_start();
+                    $usuario_validado = false;
+                    if(isset($_GET['login']) && $_GET['login'] == "erro"){        
+                ?>
+                        <div class = "error">
+                            <p>Usuario ou senha Inválido(s)</p>
+                        </div>
+                <?php
+                    }
+                ?>
+                <button type="submit" class = "botao">Entrar</button>
+                <button type="button" class = "botao btn1"><a href="Cadastro.html">Cadastrar</a></button>
+                <input type="radio" name="ProprietarioLocador" value = "Proprietario" class = "radio_oculto" checked>
+            </form>
+        </div>
+        <div class="form-container sign-in">
+            <form action="ValidaLogin.php" method="post">
+                <img src="img/WorkWaveLogoSemFundo.png" width = "180" class = "imagem">
+                <input type="email" name="login" placeholder = "Email">
+                <input type="password" name="senha" placeholder = "Senha">
+                <?php
+                    if(isset($_GET['login']) && $_GET['login'] == "erro"){        
+                ?>
+                        <div class = "error">
+                            <p>Usuario ou senha Inválido(s)</p>
+                        </div>
+                <?php
+                    }
+                ?>
+                <button type="submit" class = "botao">Entrar</button>
+                <button type="button" class = "botao btn1"><a href="Cadastro.html">Cadastrar</a></button>
+                <input type="radio" name="ProprietarioLocador" value = "Locatario" class = "radio_oculto" checked>
+            </form>
+        </div>
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <h1>Faça Login como Proprietario</h1>
+                    <p>ou Entre como Locatario</p>
+                    <button class="hidden" id = "Voltar">Locatario</button>
+                </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>Faça Login como Locatario</h1>
+                    <p>Ou Entre como Proprietario</p>
+                    <button class="hidden" id = "Empresa">Proprietario</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src = "js/script.js"></script>
+</body>
+</html>
