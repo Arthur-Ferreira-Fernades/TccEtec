d110ee039869613a2d8ee7a9edcf3700a1bb8bf2
Guinds <167483241+Guinds@users.noreply.github.com>
2024-05-07 22:13:22 -0300

    3.1
    rodape

--- SobreNos.php
+++ SobreNos.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/SobreNos.css">
-    <title>Home</title>
+    <title>SobreNos</title>
 
     
 </head>
@@ -77,6 +77,31 @@
             </div>
         </div>
     </div>
+
+    <footer class="rodape" id="contato">
+        <div class="rodape-div">
+
+            <div class="logo">
+                <img src="img/WorkWave-removebg-preview (1).png" alt="" width="95px" height="95px">
+                <img src="img/WorkWave__2_-removebg-preview.png" alt="" width="100px" height="100px">
+            </div>
+
+            <div class="Social">
+                <div class="rodape-div-3-coluna">
+                <a class="btn" href="#">
+                    <img src="img/instagram.png" alt="" width="40" height="40">
+                </a>
+                <a class="btn" href="#">
+                    <img src="img/linkedin.png" alt="" width="40" height="40">
+                </a>
+                <a class="btn" href="#" >
+                    <img src="img/github.png" alt="" width="40" height="40">
+                </a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
